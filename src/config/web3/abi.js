cd85a02afe8cfcec25c57896888d221909e9f8ff
kalle <zxcvbnm@.y1>
2024-10-06 17:38:58 +0800

    fix：页面提交

--- src/config/web3/abi.js
+++ src/config/web3/abi.js
@@ -1,3 +1,20 @@
+// export const ERC20_ABI = [
+// 	{
+// 		'constant': true,
+// 		'inputs': [{ 'name': '_owner', 'type': 'address' }],
+// 		'name': 'balanceOf',
+// 		'outputs': [{ 'name': 'balance', 'type': 'uint256' }],
+// 		'type': 'function',
+// 	},
+// 	{
+// 		'constant': true,
+// 		'inputs': [],
+// 		'name': 'decimals',
+// 		'outputs': [{ 'name': '', 'type': 'uint8' }],
+// 		'type': 'function',
+// 	},
+// ]
+
 export const ERC20_ABI = [
 	// 授权某地址消费用户代币的方法
 	{
